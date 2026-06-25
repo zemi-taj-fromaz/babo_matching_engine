f3061769b6008ad2b2189ffe87b498cebe4a2dd8
Hrvoje Ljubas <hrco.ljubas@gmail.com>
2026-06-25 20:03:39 +0200

    copy of liquibook and fastmap explore

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main()
   spdlog::info("First breath!");
   spdlog::warn("Matching engine starting up...");
   spdlog::debug("This won't show unless you lower the level");
-
+ // alou
   // Lower the level to see debug/trace messages:
   spdlog::set_level(spdlog::level::debug);
   spdlog::debug("Now debug is visible");
